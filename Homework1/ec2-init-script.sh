a531098f7a5c37c9ea37742dacdc9d6360fbad97
KostaGorod <korolx147@gmail.com>
2023-01-24 07:31:31 +0000

    delete legacy files

--- Homework1/ec2-init-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo "<html><head></head><body><p style=\"text-align: center;\">Welcome to Grandpa's Whiskey</p></body></html>" | sudo tee /usr/share/nginx/html/index.html
